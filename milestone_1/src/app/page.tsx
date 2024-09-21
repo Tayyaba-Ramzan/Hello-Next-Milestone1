a0f9f40de3182fb317b937128f99687aca27f1ed
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-09-21 16:13:26 -0700

    Set up Next.js and created your first app.

---
+++ milestone_1/src/app/page.tsx
@@ -0,0 +1,13 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+
+export default function Home() {
+  return (
+    <div className="container">
+      <h1 className="hello">
+        Hello World!
+      </h1>
+      <div className="footer">Designed with care by Tayyaba Ramzan</div>
+    </div>
+  );
+}
